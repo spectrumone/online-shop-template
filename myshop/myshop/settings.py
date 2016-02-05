4e2e6c088cb2235184ca0040f8298fde1863f44c
Antonio Carlos L. Ortiz <ortizantoniocarlos@gmail.com>
2016-02-05 12:34:53 +0800

    added class for shopping cart

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -104,3 +105,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
