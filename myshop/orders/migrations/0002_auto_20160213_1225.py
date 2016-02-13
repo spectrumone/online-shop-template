76a05967a411acc4a4a1bdf54bd7552372231202
Nadine Jamila <nadinejamila@gmail.com>
2016-02-13 20:25:43 +0800

    update paid field in Order model

---
+++ myshop/orders/migrations/0002_auto_20160213_1225.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='order',
+            name='paid',
+            field=models.BooleanField(default=False),
+        ),
+    ]
--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -11,7 +11,7 @@ class Order(models.Model):
     city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=True)
+    paid = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('-created',)
